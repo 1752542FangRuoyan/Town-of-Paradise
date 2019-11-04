5b6f19e4aa403a9c3fa585878844d695bfb0226c
Cyeris <is365s@icloud.com>
2019-11-04 13:37:36 +0800

    First Facade

--- src/MyFarm/Facade.java
+++ src/MyFarm/Facade.java
@@ -1,20 +1,31 @@
 package MyFarm;
 
-import MyFarm.creature.Creature;
+import MyFarm.animals.Pig;
+import MyFarm.creature.CreatureModule;
 import MyFarm.menu.AnimalMenu;
+import MyFarm.menu.PlantMenu;
+import MyFarm.plants.BasePlant;
 
 public class Facade {
-    static private AnimalMenu animals;
-    public Facade(AnimalMenu animals){
-        this.animals=animals;
+    private PlantMenu plantMenu;
+    private AnimalMenu animalMenu;
+    
+    public Facade(PlantMenu pm, AnimalMenu am){
+        plantMenu=pm;
+        animalMenu=am;
     }
-    static public void feedAll(){
-        for (Creature x:animals.creatures
-             ) {
-            x.getFed(Timer.getCurrentTime());
-        }
+    
+    public void plantCrop(String name){
+        BasePlant plant = (BasePlant) CreatureModule.MyClone("Crop");
+        plant.setName(name);
+        plantMenu.addPlant(plant);
+        System.out.println("新生成了" + plant.getName());
     }
-    static public void showHunger(String name){
-        System.out.println(name+"is hungry.");
+
+    public void newPig(String name){
+        Pig pig = (Pig) CreatureModule.MyClone("Pig");
+        pig.setName(name);
+        animalMenu.addAnimal(pig);
+        System.out.println("新生成了" + pig.getName());
     }
 }
